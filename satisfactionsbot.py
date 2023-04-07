742298c7339d068aca441334e7288dd2bbfc6bff
Carlos M <carlosmendez.2021@outlook.com>
2023-04-07 07:28:14 -0500

    Added a commands cog with a test command

--- satisfactionsbot.py
+++ satisfactionsbot.py
@@ -1,5 +1,7 @@
 import os
+import asyncio
 import discord
+import botcommands
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -14,5 +16,11 @@ intents.message_content = True
 
 # Set Bot
 bot = commands.Bot(command_prefix=CMD_PREFIX, intents=intents)
+
+# Load Cogs
+async def add_cogs():
+    await bot.add_cog(botcommands.BotCommands(bot))
+
+asyncio.run(add_cogs())
 
 bot.run(DISCORD_TOKEN)
